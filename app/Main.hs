2591f7ef9c66a58ba89a9029451fdedcea0ae9f1
odmishien <tmtm.yagi.007@gmail.com>
2020-05-25 02:16:22 +0900

    gogo

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import           Data.Time
 
 main :: IO ()
-main = someFunc
+
+main = do
+    start <- getCurrentTime
+    print "press Enter to stop your stopwatch!"
+    input <- getChar
+    now   <- getCurrentTime
+    putStrLn $ formatTime defaultTimeLocale "%h:%m:%s" (diffUTCTime now start)
