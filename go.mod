8d19239ee70e1acffdf17f845f1d8682d2cafd68
odmishien <tmtm.yagi.007@gmail.com>
2020-06-02 16:22:05 +0900

    spreadsheetに自動でコピペしてくれる&toml読む

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/odmishien/sw2ss
 
 go 1.13
+
+require (
+	cloud.google.com/go v0.57.0 // indirect
+	github.com/BurntSushi/toml v0.3.1
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20200528225125-3c3fba18258b // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	golang.org/x/sys v0.0.0-20200523222454-059865788121 // indirect
+	google.golang.org/api v0.26.0
+	google.golang.org/genproto v0.0.0-20200601130524-0f60399e6634 // indirect
+)
--- main.go
+++ main.go
@@ -2,21 +2,80 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"time"
+
+	"github.com/BurntSushi/toml"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
 )
+
+type Config struct {
+	Sheet SheetConfig
+}
+
+type SheetConfig struct {
+	SpreadSheetID string `toml:"spreadSheetID"`
+	UpdateCell    string `toml:"updateCell"`
+}
+
+func readConfig() (Config, error) {
+	var config Config
+	_, err := toml.DecodeFile("config.toml", &config)
+	if err != nil {
+		return config, err
+	}
+	return config, nil
+}
+
+func getDuration(start time.Time, end time.Time) string {
+	duration := end.Sub(start)
+	hours := int(duration.Hours()) % 24
+	mins := int(duration.Minutes()) % 60
+	secs := int(duration.Seconds()) % 60
+	return fmt.Sprintf("%d:%d:%d\n", hours, mins, secs)
+}
 
 func main() {
 	start := time.Now()
 	fmt.Printf("\x1b[36m%s\x1b[0m", "press Enter to stop your stopwatch!\n")
 	bufio.NewScanner(os.Stdin).Scan()
 	end := time.Now()
-	diff := end.Sub(start)
+	duration := getDuration(start, end)
+	fmt.Printf("result: %s \n", duration)
 
-	hours := int(diff.Hours()) % 24
-	mins := int(diff.Minutes()) % 60
-	secs := int(diff.Seconds()) % 60
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+	config, err := google.JWTConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	ctx := context.Background()
+	srv, err := sheets.New(config.Client(ctx))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
 
-	fmt.Printf("result: %d:%d:%d\n", hours, mins, secs)
+	conf, err := readConfig()
+	if err != nil {
+		log.Fatalf("Unable to read config.toml: %v", err)
+	}
+	spreadSheetID := conf.Sheet.SpreadSheetID
+	updateCell := conf.Sheet.UpdateCell
+	updateValue := &sheets.ValueRange{
+		Values: [][]interface{}{
+			{duration},
+		},
+	}
+	_, err = srv.Spreadsheets.Values.Update(spreadSheetID, updateCell, updateValue).ValueInputOption("USER_ENTERED").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet. %v", err)
+	}
+	fmt.Printf("Success to update cell!\nIf you'd like to confirm the sheet, access:\nhttps://docs.google.com/spreadsheets/d/%s/edit#gid=0\n", spreadSheetID)
 }
